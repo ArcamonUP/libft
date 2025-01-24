f50234e2083cbac0615888d64c8ce9f7289fde8e
ArcamonUP <158064084+ArcamonUP@users.noreply.github.com>
2025-01-24 15:33:07 +0100

    Improved my very ugly split
    He is now clean

--- ft_split.c
+++ ft_split.c
@@ -6,79 +6,96 @@
 /*   By: kbaridon <kbaridon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:53:45 by kbaridon          #+#    #+#             */
-/*   Updated: 2024/11/13 13:38:43 by kbaridon         ###   ########.fr       */
+/*   Updated: 2025/01/24 15:28:11 by kbaridon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count_tab(const char *str, char c)
+int	count_words(const char *str, char sep)
 {
 	int	i;
-	int	count;
+	int	result;
 
 	i = 0;
-	count = 0;
+	result = 0;
 	while (str[i])
 	{
-		while (str[i] && str[i] == c)
+		while (str[i] && str[i] == sep)
 			i++;
 		if (str[i])
-			count++;
-		while (str[i] && str[i] != c)
+			result++;
+		while (str[i] && str[i] != sep)
 			i++;
 	}
-	return (count);
+	return (result);
 }
 
-static void	*free_tab(int x, char **tab)
+void	free_tab(char **tab, int nb)
 {
-	if (x == 0)
-		free(tab);
-	else
+	int	i;
+
+	i = 0;
+	while (i < nb)
+		free(tab[i++]);
+	free(tab);
+}
+
+char	*get_word(const char *str, char sep)
+{
+	char	*result;
+	int		i;
+	int		count;
+
+	count = 0;
+	while (str[count] && str[count] != sep)
+		count++;
+	result = malloc(sizeof(char) * (count + 1));
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < count)
 	{
-		while (x >= 0)
-		{
-			free(tab[x]);
-			x--;
-		}
-		free(tab);
+		result[i] = str[i];
+		i++;
 	}
-	return (NULL);
+	result[i] = '\0';
+	return (result);
 }
 
-void	init_tab(int tab[3])
+char	**get_tab(char **result, const char *str, char sep)
 {
-	tab[0] = 0;
-	tab[1] = 0;
-	tab[2] = 0;
+	int	i;
+	int	id;
+
+	i = 0;
+	id = 0;
+	while (str[i])
+	{
+		if (str[i] == sep)
+			i++;
+		else
+		{
+			result[id] = get_word(str + i, sep);
+			if (!result[id])
+				return (free_tab(result, id), NULL);
+			id++;
+			while (str[i] && str[i] != sep)
+				i++;
+		}
+	}
+	result[id] = NULL;
+	return (result);
 }
 
 char	**ft_split(const char *str, char c)
 {
 	char	**result;
-	int		tab[3];
+	int		tmp;
 
-	init_tab(tab);
-	result = malloc(sizeof(char *) * (ft_count_tab(str, c) + 1));
+	tmp = count_words(str, c);
+	result = malloc(sizeof(char *) * (tmp + 1));
 	if (!result)
-		return (free_tab(tab[2], result));
-	while (str[tab[0]])
-	{
-		if (str[tab[0]] == c)
-			tab[0]++;
-		else
-		{
-			tab[1] = tab[0];
-			while (str[tab[0]] && str[tab[0]] != c)
-				tab[0]++;
-			result[tab[2]] = malloc(sizeof(char) * (tab[0] - tab[1] + 1));
-			if (!result[tab[2]])
-				return (free_tab(tab[2], result));
-			ft_strlcpy(result[tab[2]], str + tab[1], tab[0] - tab[1] + 1);
-			tab[2]++;
-		}
-	}
-	result[tab[2]] = NULL;
-	return (result);
+		return (NULL);
+	return (get_tab(result, str, c));
 }
